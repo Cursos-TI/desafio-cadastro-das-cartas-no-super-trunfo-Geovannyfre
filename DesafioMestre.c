b38b90def79576ced8405e093739f3ba24ec2c5d
Geovannyfre <geovanny.skta@gmail.com>
2025-03-11 10:43:59 +0000

    DesafioDoMestre

---
+++ DesafioMestre.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main () {
+
+    char produtoA[30] = "Produto A";
+    char produtoB[30] = "Produto B";
+
+    unsigned int estoqueA = 1000;
+    unsigned int estoqueB = 2000;
+
+    float valorA = 10.50;
+    float valorB = 20.40; 
+
+    unsigned int estoqueMinimoA = 500;
+    unsigned int estoqueMaximoB = 2500;
+
+    double valorTotalA;
+    double valorTotalB;
+
+    int resultadoA;
+    int resultadoB;
+    
+    printf("Produto %s tem estoque %u e o valor unitario é R$ %.2f\n",produtoA,estoqueA,valorA);
+    printf("Produto %s tem estoque %u e o valor unitario e R$ %.2f\n",produtoB,estoqueB,valorB);
+
+    //comparação com o valor mínimo do estoque
+    resultadoA = estoqueA > estoqueMinimoA;
+    resultadoB = estoqueB > estoqueMaximoB;
+
+    printf("O produto %s tem estoque minimo %d\n",produtoA,resultadoA);
+    printf("O produto %s tem estoque minimo %d\n",produtoB,resultadoB);
+
+    //comparação entre os valores totais dos produtos
+    printf("O valor total de A (R$ %.2f) e maior que o valor total de B (R$ %.2f)? %d\n",
+                                 estoqueA * valorA,
+                                 estoqueB * valorB,    
+                                (estoqueA * valorA) > (estoqueB * valorB));
+    }                                
